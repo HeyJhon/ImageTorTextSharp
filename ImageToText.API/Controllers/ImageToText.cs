cf4fd7222e353f8cdd4d6ae7b8436ef4e3d94a73
HeyJhon <srjhonxy@gmail.com>
2024-06-12 21:44:32 -0600

    Add project files.

---
+++ ImageToText.API/Controllers/ImageToText.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using Tesseract;
+
+namespace ImageToText.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageToText : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            //validar el formato de la imagen
+            if (file == null || (file.ContentType != "image/png" && file.ContentType != "image/jpeg"))
+                return BadRequest("Error");
+
+            string resultText = "";
+            using( var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                byte[] imageBytes = memoryStream.ToArray();
+                resultText = ConvertImageToText(imageBytes);
+            }
+            return Ok(resultText);
+
+        }
+
+        private string ConvertImageToText(byte[] arrayImage)
+        {
+            //Logica de Tesseract
+            var engine = new TesseractEngine("tessdata", "spa", EngineMode.Default);
+            var image = Pix.LoadFromMemory(arrayImage);
+            var page = engine.Process(image);
+
+            string text = page.GetText();
+            return text;
+        }
+
+        
+    }
+}
---
+++ ImageToText.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tesseract;
+
+string pathImage = @"C:\Utils\reymidasscan.jpg";
+string pathData = @"C:\Utils\tessdata";
+var engine = new TesseractEngine(pathData, "spa", EngineMode.Default);
+var image = Pix.LoadFromFile(pathImage);
+var page = engine.Process(image);
+
+string text = page.GetText();
+Console.WriteLine("El texto es:");
+Console.WriteLine(text);
